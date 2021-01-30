fa45a7574cec1036289328710560b6309588cb8c
Jonathon Waters <jonathon.waters@tekever.com>
2021-01-30 21:52:06 +0000

    Initial Commit

---
+++ S3FileUploader/BucketManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace S3FileUploader
+{
+	public class BucketManager
+	{
+		public ObservableCollection<S3Bucket> Buckets { get; set; }
+
+		public BucketManager()
+		{
+			Buckets = new ObservableCollection<S3Bucket>();
+		}
+
+		public async Task GetBucketList(string awsAccessKeyId, string secretAccessKey)
+		{
+			Buckets.Clear();
+
+			AmazonS3Client client = new AmazonS3Client(awsAccessKeyId, secretAccessKey, Amazon.RegionEndpoint.EUWest2);
+
+			ListBucketsResponse response = await client.ListBucketsAsync();
+			foreach (S3Bucket bucket in response.Buckets)
+			{
+				Buckets.Add(bucket);
+			}
+		}
+	}
+}
---
+++ S3FileUploader/Credentials.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S3FileUploader
+{
+	public class Credentials
+	{
+		public string AccessKeyId { get; set; }
+		public string SecretAccessKey { get; set; }
+	}
+}
